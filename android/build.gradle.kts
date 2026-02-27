1847f502fb4aba903a1f617497ae60ec8d7a6703
mti1122hj <mti1122hj@gmail.com>
2026-02-27 23:23:24 +0900

    ビルド失敗の対策

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,27 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // Workaround for plugins that don't specify a namespace (required by AGP 8+).
+    // This applies to isar_flutter_libs and other legacy plugins.
+    project.pluginManager.withPlugin("com.android.library") {
+        val android = project.extensions.getByName("android")
+            as com.android.build.gradle.LibraryExtension
+        if (android.namespace.isNullOrEmpty()) {
+            val manifest = project.file("src/main/AndroidManifest.xml")
+            if (manifest.exists()) {
+                val packageName = javax.xml.parsers.DocumentBuilderFactory
+                    .newInstance()
+                    .newDocumentBuilder()
+                    .parse(manifest)
+                    .documentElement
+                    .getAttribute("package")
+                if (packageName.isNotEmpty()) {
+                    android.namespace = packageName
+                }
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
